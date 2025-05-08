987d88a03978f02fcf69552673ddf148688a4f88
mbathio <mbathiobadiallo20@gmail.com>
2025-05-08 23:23:05 +0200

    Premier commit

--- MyAPP/www/js/index.js
+++ MyAPP/www/js/index.js
@@ -1,32 +1,53 @@
 window.onload = function() {
     const imcButton = document.querySelector('button');
-    imcButton.onclick = function() {
-       const poids = document.querySelector('#Poids');
-       const taille = document.querySelector('#Hauteur');
-       const imc = poids.value / Math.pow(taille.value, 2);
-       let interpretation = 'Vous etes ';
+    imcButton.onclick = function(e) {
+        e.preventDefault(); // Empêcher le rechargement de la page
+        const poids = document.querySelector('#Poids');
+        const taille = document.querySelector('#Hauteur');
+        
+        // Vérifier que les valeurs sont renseignées
+        if(!poids.value || !taille.value) {
+            alert('Veuillez remplir tous les champs');
+            return;
+        }
+        
+        // Convertir la taille de cm en m
+        const tailleEnMetres = taille.value / 100;
+        
+        // Calculer l'IMC
+        const imc = (poids.value / Math.pow(tailleEnMetres, 2)).toFixed(1);
+        
+        // Déterminer l'interprétation
+        let interpretation = 'Vous êtes ';
+        
+        if(imc < 18.5){
+            interpretation += 'en sous-poids';
+        }
+        else if(imc < 24.9){
+            interpretation += 'de poids normal';
+        }
+        else if(imc < 29.9){
+            interpretation += 'en surpoids';
+        }
+        else{
+            interpretation += 'en situation d\'obésité';
+        }
+        
+        // Afficher le résultat
+        const resultatBox = document.querySelector('#result');
+        resultatBox.innerHTML = `Votre IMC est de ${imc} <hr> ${interpretation}`;
+        
+        // Afficher/masquer le bouton si nécessaire
+        toggleButton();
+    };
+};
+
+function toggleButton(){
+    const imcButton = document.querySelector('button');
+    // Si vous souhaitez cacher puis réafficher le bouton
+    if(imcButton.style.display === 'none') {
+        imcButton.style.display = 'block';
+    } else {
+        imcButton.style.display = 'block'; // Maintenir le bouton visible
     }
-    if(imc < 18.5){
-        interpretation += 'sous-poids';
-    }
-    else if(imc < 24.9){
-        interpretation += 'sain';
-    }
-    else if(imc < 29.9){
-        interpretation += 'surs-poids';
-    }
-    else{
-        interpretation += 'obes';
-    }
-    const resultatBox = document.querySelector('#result');
-    resultatBox.innerHTML = `votre IMC est de ${imc} <hr> ${interpretation}`;
-    hideButton();
-    
 }
-
-function hideButton(){
-    imcButton.style.display = 'none';
-    imcButton.style.display = 'block';
-
-    
-}   
